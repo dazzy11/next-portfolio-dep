37234ce81ac5c6f7eb9c05480d449221ba57b94c
Deepak madhukumar <deepakmadhukumar1124@gmail.com>
2025-04-05 01:47:46 +0530

    first commit

---
+++ src/Sample.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sam.css';
+const SampleComponent: React.FC = () => {
+    return (
+        <div>
+            <h1 className='hi'>Hello, Weather App!</h1>
+            <p>This is a simple React component.</p>
+        </div>
+    );
+};
+
+export default SampleComponent;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 //import WeatherApp from './WeatherApp.tsx'
 import Portfolio from './Portfolio.tsx'
+//import Sample from './sample.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
