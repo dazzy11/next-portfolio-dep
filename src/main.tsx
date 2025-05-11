618f06700c2599658c9298ef2252d7d6b2aac742
dazzy11 <deepakmadhukumar1124@gmail.com>
2025-05-11 13:14:59 +0530

    new version

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Index from './Index'
 //import WeatherApp from './WeatherApp.tsx'
 //import Portfolio from './Portfolio.tsx'
-import Dport from './dport.tsx'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Dport/>
+    <Index/>
   </StrictMode>,
 )
