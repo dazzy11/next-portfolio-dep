ee6a9cc5affd41cfaf64ec02ae439d96b1a0bde9
dazzy11 <deepakmadhukumar1124@gmail.com>
2025-05-22 15:47:10 +0530

    analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Index from './Index'
 import CustomCursor from './CustomCursor'
+import { inject } from '@vercel/analytics';
+ 
+inject();
 //import CursorTrail from './CursorTrail'
 //import WeatherApp from './WeatherApp.tsx'
 //import Portfolio from './Portfolio.tsx'
